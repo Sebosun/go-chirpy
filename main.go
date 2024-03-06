b66cb11c4556294ef96c62163c1e4ab619ea8201
Sebosun <sebos@protonmail.com>
2024-03-06 11:21:04 +0100

    feat: /healtz endpoint

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
+	const port = "8080"
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
-	log.Fatal(http.ListenAndServe(":8080", corsMux))
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app", fs)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type: text/plain; charset=utf-8", "*")
+		var isOk []byte = []byte("OK")
+		w.Write(isOk)
+
+	})
+
+	fmt.Printf("Serving on port %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
